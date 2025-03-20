610773e518c9ced6065fb9861257a977df193b8b
Roza Mesfin <rozitamine70@gmail.com>
2025-03-20 09:46:01 +0300

    welcome update

--- gpa_calculator/lib/Welcome.dart
+++ gpa_calculator/lib/Welcome.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'HomeScreen.dart';
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Welcome', style: Theme.of(context).textTheme.titleLarge),
-        centerTitle: true,
-      ),
+    
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // App Logo or Icon
+            
             Icon(
               Icons.calculate,
               size: 100,
@@ -21,7 +18,6 @@ class WelcomeScreen extends StatelessWidget {
             ),
             SizedBox(height: 20),
 
-            // Welcome Title
             Text(
               'Welcome to the GPA/CGPA Calculator!',
               style: Theme.of(context).textTheme.displayLarge,
@@ -29,27 +25,29 @@ class WelcomeScreen extends StatelessWidget {
             ),
             SizedBox(height: 10),
 
-            // Subtitle
             Text(
               'Track and calculate your academic performance with ease.',
               style: Theme.of(context).textTheme.bodyMedium,
               textAlign: TextAlign.center,
             ),
             SizedBox(height: 40),
-
-            // Get Started Button
             ElevatedButton(
               onPressed: () {
-                // Navigate to the next screen
-              },
+                 Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()), 
+                  );
+
+                              },
               style: ElevatedButton.styleFrom(
                 padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
               ),
               child: Text(
                 'Get Started',
-                style: Theme.of(context).textTheme.labelLarge,
+              
+              )
               ),
-            ),
+            
           ],
         ),
       ),
